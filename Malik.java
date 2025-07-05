1133997bbfaaaacafa7f3003ed8b80dc4942bb06
Rehan malik <malikr96204@gmail.com>
2025-07-06 00:33:22 +0530

    2nd ommitfrom home

--- Malik.java
+++ Malik.java
@@ -3,6 +3,8 @@ package HomeLo;
 public class Malik {
     public static void main(String[] args) {
         System.out.println("from home");
+
+        System.out.println(" my 2n commad");
     }
     
 }
